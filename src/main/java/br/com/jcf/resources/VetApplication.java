e688682ab630820260f45a4beacb20db52a3d7ef
Faissal <Faissal>
2016-07-03 19:58:52 -0300

    first commit

---
+++ src/main/java/br/com/jcf/resources/VetApplication.java
@@ -0,0 +1,9 @@
+package br.com.jcf.resources;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class VetApplication extends Application {
+
+}
