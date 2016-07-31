dcc0ab6812bd7ae42d51270b4e3d71b9f076c22e
Faissal <Faissal>
2016-07-31 16:25:25 -0300

    ajustes

--- src/main/java/br/com/jcf/vet/entity/Dono.java
+++ src/main/java/br/com/jcf/vet/entity/Dono.java
@@ -5,8 +5,6 @@ import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -84,7 +82,7 @@ public class Dono implements IEntity<String> {
 		this.cpf = cpf;
 	}
 
-	@XmlElement(type = Date.class)
+	@XmlElement
 	@Column(name="DT_NASCIMENTO")
 	@Temporal(TemporalType.DATE)
 	public Date getDtNascimento() {
